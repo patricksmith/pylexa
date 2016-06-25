f099d692b4fb3e757f89840ec821f6a3a5506e94
Patrick Smith <patrick@promptworks.com>
2016-06-25 13:45:04 -0400

    Release v1.0.0

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ except(IOError, ImportError):
 
 setup(
     name = 'pylexa',
-    version = '0.0.15',
+    version = '1.0.0',
     author = 'Patrick Smith',
     author_email = 'pjs482@gmail.com',
     description = ('A library to ease creation of an Alexa Skills Kit'),
